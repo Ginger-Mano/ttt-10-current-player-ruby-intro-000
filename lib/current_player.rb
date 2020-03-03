34ab554b62e06dd9415c5b49af890213554d3a73
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-03 19:39:41 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,10 @@
 def turn_count(board)
   counter = 0
   players = ["player 1", "player 2"]
-  players.each do |player|
-    if players == "X" || "O"
-      counter += 1
+  if the board reads X or O, then other player goes
+    counter += 1
  end
+end
+
+def current_player
 end

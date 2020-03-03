c3506c8f1b23d3bab682d37c20e0d998818a7809
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-03 20:40:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 require 'pry'
 def turn_count(board)
   counter = 0
-#  players = ["player 1", "player 2"] 1 
+#  players = ["player 1", "player 2"] 1
 binding.pry
-  board.each do |player|  
+  board.each do |player|
     "X" || "O"
     counter += 1
  end
